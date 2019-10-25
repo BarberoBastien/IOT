85cdec1ffad4c4c6deea54133fceca4ddcee3c04
Barbero <bastienbarbero@gmail.com>
2019-10-25 10:55:24 +0200

    Material

--- IOT-APP/src/app/app.module.ts
+++ IOT-APP/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { IndexBannerComponent } from './index-banner/index-banner.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    IndexBannerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [IndexBannerComponent]
 })
 export class AppModule { }
