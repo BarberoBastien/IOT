b1834fea820ba53c793ef302c94f13f223aa39f4
Barbero <bastienbarbero@gmail.com>
2019-10-25 11:43:44 +0200

    Card

--- IOT-APP/src/app/app.module.ts
+++ IOT-APP/src/app/app.module.ts
@@ -3,12 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { IndexBannerComponent } from './index-banner/index-banner.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { CardComponent } from './card/card.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [
-    IndexBannerComponent
+    IndexBannerComponent,
+    CardComponent
   ],
   imports: [
+    MatGridListModule,
+    MatCardModule,
+    MatToolbarModule,
     BrowserModule,
     BrowserAnimationsModule
   ],
